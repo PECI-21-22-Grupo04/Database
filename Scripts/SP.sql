9d914e352dad86a34c620a9b5d737d22b4274eb6
diogomaduro97 <diogomaduro97@gmail.com>
2022-05-13 21:48:22 +0100

    selectclientfromid added, and tests seperated from SP script

--- Scripts/SP.sql
+++ Scripts/SP.sql
@@ -316,7 +316,8 @@ BEGIN
     ELSE
 		SELECT userID INTO @iID FROM (SELECT userID, email FROM PECI_PROJ.SysUser) AS t1 WHERE CONVERT(AES_DECRYPT(t1.email, dbKey) USING utf8) = INInstructorEmail;
 		SELECT * 
-			FROM (SELECT 	CONVERT(AES_DECRYPT(email, dbKey) USING utf8) AS mail,
+			FROM (SELECT 	userID AS clientID,
+							CONVERT(AES_DECRYPT(email, dbKey) USING utf8) AS mail,
 							CONVERT(AES_DECRYPT(firstName, dbKey) USING utf8) AS firstName,
 							CONVERT(AES_DECRYPT(firstName, dbKey) USING utf8) AS lastName,
 							birthdate,
@@ -325,6 +326,28 @@ BEGIN
 			FROM ((SELECT * FROM PECI_PROJ.Affiliation INNER JOIN PECI_PROJ.AffiliationLog ON PECI_PROJ.Affiliation.affiliationID = PECI_PROJ.AffiliationLog.affID) AS res
 				INNER JOIN PECI_PROJ.SysUser ON  PECI_PROJ.SysUser.userID = res.affClientID) 
 				WHERE canceledDate IS NULL AND affInstID = @iID) AS finalTbl;   
+	END IF;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE spSelectInstructorClientsFromID (IN INInstructorEmail NVARCHAR(255),IN INclientID INT, IN dbKey NVARCHAR(255))
+BEGIN
+	IF ((SELECT COUNT(*) FROM (SELECT userID, email FROM PECI_PROJ.SysUser) AS t1 WHERE CONVERT(AES_DECRYPT(t1.email, dbKey) USING utf8) = INInstructorEmail) <> 1) THEN
+		CALL spRaiseError();
+    ELSE
+		SELECT userID INTO @iID FROM (SELECT userID, email FROM PECI_PROJ.SysUser) AS t1 WHERE CONVERT(AES_DECRYPT(t1.email, dbKey) USING utf8) = INInstructorEmail;
+		SELECT * 
+			FROM (SELECT 	userID AS clientID,
+							CONVERT(AES_DECRYPT(email, dbKey) USING utf8) AS mail,
+							CONVERT(AES_DECRYPT(firstName, dbKey) USING utf8) AS firstName,
+							CONVERT(AES_DECRYPT(firstName, dbKey) USING utf8) AS lastName,
+							birthdate,
+							sex,
+							DATE(signedDate) AS clientSince	
+			FROM ((SELECT * FROM PECI_PROJ.Affiliation INNER JOIN PECI_PROJ.AffiliationLog ON PECI_PROJ.Affiliation.affiliationID = PECI_PROJ.AffiliationLog.affID) AS res
+				INNER JOIN PECI_PROJ.SysUser ON  PECI_PROJ.SysUser.userID = res.affClientID) 
+				WHERE canceledDate IS NULL AND affInstID = @iID AND affClientID = INclientID ) AS finalTbl;   
 	END IF;
 END $$
 DELIMITER ;
@@ -477,58 +500,3 @@ INSERT INTO PECI_PROJ.Program (pName, pDescription, forPathology, thumbnailPath,
 INSERT INTO PECI_PROJ.Program (pName, pDescription, forPathology, thumbnailPath, videoPath, isPublic, creatorIntsID) VALUES ('defaultPogram2', 'Do Yoga program 1', '', 'thumbnailpath/here', 'videopath/here', 1, null);
 
 
--- TESTES MOBILE APP --
-CALL spCreateClient('client@mail.com','teste','1234', '1999-01-01', 'M', 'rua', '3000-500', 'cidade', 'pais', 'chave');
-CALL spCreateClient('clientNumber2@mail.com','testeNumber2','4321', '2005-01-01', 'M', 'rua', '5000-600', 'cidade2', 'pais2', 'chave');
-CALL spSelectClient('client@mail.com','chave');
-
-CALL spAddClientInfo('client@mail.com', 180, 80, 'begginer', 20, 'pathologies', 'chave');
-CALL spAddClientInfo('client@mail.com', 185, 85, 'intermediate', 20, 'pathologies', 'chave');
-CALL spAddClientInfo('client@mail.com', 190, 90, 'advanced', 20, 'pathologies', 'chave');
-CALL spSelectClientInfo('client@mail.com','chave');
-
-CALL spFinalizeClientPayment('client@mail.com', 'monthly', 49.99, 'chave');
-CALL spFinalizeClientPayment('client@mail.com', 'yearly ', 199.99, 'chave');
-CALL spSelectClientPaymentHistory('client@mail.com', 'chave');
-
-CALL spSelectAvailableInstructors('chave');
-
-CALL spAssociateInstructor('client@mail.com', 'instructor@mail.com', 'chave');
-CALL spAssociateInstructor('client@mail.com', 'instructorNumber2@mail.com', 'chave');
-CALL spSelectClientInstructorHistory('client@mail.com', 'chave');
-
-CALL spClientReviewInstructor('client@mail.com', 'instructor@mail.com', 3, null, 'chave');
-CALL spClientReviewInstructor('client@mail.com', 'instructorNumber2@mail.com', 5, 'Very good, has the best plans!', 'chave');
-
-CALL spAddClientRewards('client@mail.com', 1, 'chave');
-CALL spSelectClientRewards('client@mail.com', 'chave');
-
-CALL spSelectClientPrograms('client@mail.com', 'chave');
-
-CALL spDeleteClient('client@mail.com','chave');
-
-
--- TESTES WEB APP --
-CALL spCreateInstructor('instructor@mail.com','teste','1234', '1999-01-01', 'M', 'rua', '3000-500', 'cidade', 'pais', 'contactNumber', 'paypalAccount', 0, 'chave');
-CALL spCreateInstructor('instructorNumber2@mail.com','teste','1234', '2005-02-23', 'M', 'rua', '3000-500', 'cidade', 'pais', 'contactNumber123', 'paypalAccount123', 123, 'chave');
-CALL spSelectInstructor('instructor@mail.com','chave');
-
-CALL spSelectInstructorClients('instructorNumber2@mail.com', 'chave');
-
-CALL spCreateExercise('instructor@mail.com', 'exer123', 'advanced', 'Lie on the floor and do a pushup', 'Pregnant', 'chest' ,'thumbnailpath/here', 'videopath/here', 'chave');
-CALL spCreateExercise('instructorNumber2@mail.com', '123xer', 'beginner', 'Do a pullup', '', 'back' ,'thumbnailpath/here', 'videopath/here', 'chave');
-CALL spSelectInstructorExercises('instructor@mail.com', 'chave');
-
-CALL spCreateProgram('instructor@mail.com', 'prog123', 'Very easy program', 'Pregnant', 'thumbnailpath/here', 'videopath/here', 0, 'chave');
-CALL spCreateProgram('instructor@mail.com', 'progShowcase', 'Somewhat hard', 'AVC', 'thumbnailpath/here', 'videopath/here', 1, 'chave');
-CALL spCreateProgram('instructorNumber2@mail.com', '123prog', 'idk', '', 'thumbnailpath/here', 'videopath/here', 1,'chave');
-CALL spSelectInstructorPrograms('instructor@mail.com', 'chave');
-CALL spSelectInstructorShowcasePrograms('instructor@mail.com', 'chave');
-
-CALL spAddExerciseToProgram(3, 3, 1, 1, '00:00:10');
-CALL spAddExerciseToProgram(2, 3, 1, 1, '00:00:30');
-CALL spSelectProgramExercises(3);
-CALL spAssociateProgramToClient('client@mail.com', 3, 'chave');
-
-CALL spSelectDefaultExercises();
-CALL spSelectDefaultPrograms();
